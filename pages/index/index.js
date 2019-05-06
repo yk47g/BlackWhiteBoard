dbe6a10abf1a4ebd51c20842ef4e96c40f8acb45
程IMTao <191213896@qq.com>
2019-05-06 20:04:35 +0800

    画笔功能尝试完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+   
+    nowXY:{
+     
+    }
+  
   },
 
   /**
@@ -13,34 +17,49 @@ Page({
   onLoad: function (options) {
     
   },
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+    // this.setData({
+    //   ctx:wx.createCanvasContext("testCanvas")
+    // })
+   
+    // this.data.ctx.setStrokeStyle('white')
+    // this.data.ctx.setLineWidth(1)
+    // this.data.ctx.rect(0, 0, 120, 200)
+    // this.data.ctx.stroke()
+    // this.data.ctx.draw()
+  },
 
   canvas_touchstart(e){
-    const xy = e.touches[0]
-    console.log("x:" + xy.x +"  y:"+xy.y);
+    this.setData({
+      nowXY:e.touches[0]
+      
+    })
     
   },
   canvas_touchmove(e){
-    const xy = e.touches[0]
-    console.log("x:" + xy.x + "  y:" + xy.y);
+   
+    const ctx= wx.createCanvasContext("testCanvas")
+    ctx.setStrokeStyle('blue')
+    ctx.setLineJoin("round")
+    ctx.setLineCap("round")
+    ctx.setLineWidth(5)
+    ctx.moveTo(this.data.nowXY.x, this.data.nowXY.y)
+    this.setData({
+      nowXY: e.touches[0]
+    })
+    ctx.lineTo(this.data.nowXY.x, this.data.nowXY.y)
+    
+    ctx.stroke()
+    ctx.draw(true)
   },
   canvas_touchend(e){
-    console.log(e)
-    // const xy = e.touches[0]
-    // console.log("x:" + xy.x + "  y:" + xy.y);
+   //触摸完毕
+    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-    const ctx = wx.createCanvasContext("testCanvas")
-    ctx.setStrokeStyle('white')
-    ctx.setLineWidth(1)
-    ctx.rect(0, 0,120, 200)
-    ctx.stroke()
-    ctx.draw()
-  },
 
   /**
    * 生命周期函数--监听页面显示
