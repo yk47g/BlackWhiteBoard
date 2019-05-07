86d2801cca981408476c9f524292989556bcce7a
程IMTao <191213896@qq.com>
2019-05-07 22:36:53 +0800

    数据结构基本雏形完成v0.6

--- pages/drawBoard/drawBoard.js
+++ pages/drawBoard/drawBoard.js
@@ -1,73 +1,113 @@
+
+Array.prototype.last = function () {
+  return this[this.length - 1];
+}
+class DrawBoard{
+  constructor() {
+    this.actions = [];//画布的所有绘制路径事件  
+    this.backgroundColor = "white";//默认背景颜色
+
+  }
+  addAction(){
+    this.actions.push(new Action());//添加一次绘制事件
+
+  }
+  getLastAction() {
+    return this.actions[this.actions.length-1];
+
+  }
+}
+class Action{//绘制事件类
+  constructor() {
+    this.points = [] ;//CGPoint类型
+    this.lineWidth =  5;
+    this.color = "red";
+    this.time = "";
+    this.user = "IMtao";
+  }
+  addPoint(x,y){
+    this.points.push(new CGPoint(x,y))
+  }
+  getLastPoint(index = 0){//返回CGPoint , 支持返回倒数前几个点，默认为0最后一个
+    return this.points[this.points.length - 1 - index]
+  }
+
+}
+class CGPoint {//坐标点类
+  
+  constructor(x,y) {
+    this.x = x ;
+    this.y = y;
+  }
+  getJsonArr(){
+   
+    return [this.x,this.y] 
+  }
+  getJson() {
+    const json = { x: this.x, y: this.y }
+    return json
+  }
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-   
-    actions: [],
+    drawBoard:{},
 
     actionStatus:0//工具选择状态
-  },
-  Action() {
-    this.xys = [];
-    this.time = "";
-    this.addXY = function (x, y) {
-      this.xys.push([x, y]);
+  }, 
 
-    }
-  },
+  
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    Array.prototype.last = function(){
-      return this[this.length-1];
-    }
+    this.data.drawBoard = new DrawBoard();//画布对象创建，不能直接在data创建。。
+  
   },
   /**
  * 生命周期函数--监听页面初次渲染完成
  */
   onReady: function () {
-    // this.setData({
-    //   ctx:wx.createCanvasContext("testCanvas")
-    // })
-
-    // this.data.ctx.setStrokeStyle('white')
-    // this.data.ctx.setLineWidth(1)
-    // this.data.ctx.rect(0, 0, 120, 200)
-    // this.data.ctx.stroke()
-    // this.data.ctx.draw()
+   
   },
 
   canvas_touchstart(e) {
-    let datas = this.data;
-
-    var newaction = new this.Action();
-    datas.actions.push(newaction);
-    datas.actions[datas.actions.length - 1].addXY(e.touches[0].x, e.touches[0].y)
-
     
-   
+    this.data.drawBoard.addAction();//开始添加一次绘制事件
+    let lsAction = this.data.drawBoard.getLastAction();//并且开始记录
+    lsAction.addPoint(e.touches[0].x, e.touches[0].y);
+
+
+    console.log(this.data.drawBoard)
+ 
 
   },
+
   canvas_touchmove(e) {
-    let datas = this.data;
-    const ctx = wx.createCanvasContext("testCanvas")
+
+    let ctx = wx.createCanvasContext("testCanvas");
+    let datas = this.data
+    let lsAction = datas.drawBoard.getLastAction()
+    let lsPoint = lsAction.getLastPoint();
+    let thisPoint = new CGPoint(e.touches[0].x, e.touches[0].y) 
+
     ctx.setStrokeStyle('blue')
     ctx.setLineJoin("round")
     ctx.setLineCap("round")
     ctx.setLineWidth(2)
-    console.log(datas.actions.last.xys[0])
-    return
-    ctx.moveTo(datas.actions.last.xys[0], datas.actions.last.xys[1])
-    var tempXy = [datas.actions[datas.actions.length - 2].xys[0], datas.actions[datas.actions.length - 2].xys[1]]
+
+    ctx.moveTo(...lsPoint.getJsonArr())
+   
+    ctx.lineTo(...thisPoint.getJsonArr())
+    lsAction.addPoint(...thisPoint.getJsonArr())
+
+    // ctx.quadraticCurveTo(...lsPoint.getJsonArr(), (tempXy[0] + datas.actions.last.xys[0]) / 2, (tempXy[1] + datas.actions.last.xys[1]) / 2)
     
-    datas.actions[datas.actions.length - 1].addXY(e.touches[0].x, e.touches[0].y)
-    // ctx.lineTo(this.data.nowXY.x, this.data.nowXY.y)
-    ctx.quadraticCurveTo(tempXy[0], tempXy[1], (tempXy[0] + datas.actions.last.xys[0]) / 2, (tempXy[1] + datas.actions.last.xys[1]) / 2)
     ctx.stroke()
     ctx.draw(true)
 
