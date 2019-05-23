d784159a96480a2ff209d0b4f3536f263e4c5c75
k-skye <36499136+k-skye@users.noreply.github.com>
2019-05-23 16:51:20 +0800

    样式更新

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,3 +1,5 @@
+let  app = getApp();
+
 Page({
 
     /**
@@ -8,8 +10,8 @@ Page({
         groupUser1iconUrl: "/icons/user.png",
         groupUser2iconUrl: "/icons/user.png",
         groupUser3iconUrl: "/icons/user.png",
-        ADDgroupUsericonUrl: "/icons/user.png",
-        userName: "钢铁侠",
+        ShowgetUserInfoView: false,
+        userName: "未登录",
         userId: 8888888,
         groupName: "Avengers"
     },
@@ -32,19 +34,24 @@ Page({
                 success(res) {
 
                     if (res.authSetting['scope.userInfo']) {
-                        console.log("用户已授权基本信息。")
+                        console.log("用户已授权基本信息。");
                         // 已经授权，可以直接调用 getUserInfo 获取头像昵称
                         wx.getUserInfo({
-                        success(res) {
-                            console.log(res.userInfo)
-                            app.globalData.userInfo = res.userInfo;
-                        that.setData({
-                            usericonUrl: app.globalData.userInfo.avatarUrl
-                            })
-                        }
+                            success(res) {
+                                console.log(res.userInfo);
+                                app.globalData.userInfo = res.userInfo;
+                                that.setData({
+                                    usericonUrl: app.globalData.userInfo.avatarUrl,
+                                    userName: app.globalData.userInfo.nickName
+                                });
+                            }
                         })
                     } else {
-                        console.log("用户未授权user信息")
+                        console.log("用户未授权user信息");
+                        that.setData({
+                            ShowgetUserInfoView: true,
+                            userId: "首次使用,请先登陆"
+                        });
                     }
                 }
             })
